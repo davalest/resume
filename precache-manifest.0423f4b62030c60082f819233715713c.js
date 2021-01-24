cbdb89f49920e971fa6a2d028ac630f936e427fd
David <david.valenciano@securitas.es>
2021-01-24 16:02:33 +0100

    Updates

---
+++ precache-manifest.0423f4b62030c60082f819233715713c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e5007163b6475ca8f6282914cfe215b",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "98f541cee634e2968ab5",
+    "url": "/resume/static/css/2.87a9c7a2.chunk.css"
+  },
+  {
+    "revision": "201e832b0341f2252ad8",
+    "url": "/resume/static/css/main.59392632.chunk.css"
+  },
+  {
+    "revision": "98f541cee634e2968ab5",
+    "url": "/resume/static/js/2.0f98e2e3.chunk.js"
+  },
+  {
+    "revision": "0b1121e35ce29e6fc6fa0de1284c67ed",
+    "url": "/resume/static/js/2.0f98e2e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "201e832b0341f2252ad8",
+    "url": "/resume/static/js/main.cf327af0.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  },
+  {
+    "revision": "5eff4de6204c664a754d1175afc0e8d8",
+    "url": "/resume/static/media/david.5eff4de6.jpg"
+  },
+  {
+    "revision": "653a57c6b8413b1bb43f8cbcb0952ac9",
+    "url": "/resume/static/media/resumeEn.653a57c6.pdf"
+  },
+  {
+    "revision": "bff1b89a4d01f260a9aee4f7a9d17615",
+    "url": "/resume/static/media/resumeEs.bff1b89a.pdf"
+  }
+]);
