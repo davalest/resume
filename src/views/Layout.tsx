0b93de6d9379398a87abcbab5838c51588bdb510
davidvalencianoesteban <dartvales@gmail.com>
2024-07-07 19:39:42 +0200

    testing rebase

--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -2,6 +2,7 @@ import {Outlet} from "react-router-dom";
 import Header from "../components/header/header.tsx";
 import Footer from "../components/footer/footer.tsx";
 
+
 function Layout() {
     return (
         <div style={{width:"100%"}}>
